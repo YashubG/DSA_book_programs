0a027ce7952057757252a0f0035203ad1396cc97
Yashub Goel <yashub.goel@iiitb.ac.in>
2024-12-10 19:27:17 +0530

    .

---
+++ InsertionSort.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> A(n);
+        for (int i = 0; i < n; i++)
+            cin >> A[i];
+        //The sequence will be sorted till i after each of this iteration
+        for (int i = 0; i < n; i++) {
+            int j = i;
+            while (j > 0) {
+                A.swap
+            }
+        }
+
+    }
+}
