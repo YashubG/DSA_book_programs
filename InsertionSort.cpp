d08db0056e680dff8c30ce1a7ad5a961dc0c32b6
Yashub Goel <yashub.goel@iiitb.ac.in>
2024-12-12 08:19:34 +0530

    .

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -13,12 +13,20 @@ int main()
         for (int i = 0; i < n; i++)
             cin >> A[i];
         //The sequence will be sorted till i after each of this iteration
-        for (int i = 0; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             int j = i;
-            while (j > 0) {
-                A.swap
+            int elem = A[i];
+            A.erase(A.begin() + i);
+            while (j >= 0) {
+                if (j == 0 || elem > A[j - 1]) {// This sorts in non-decreasing order.
+                    // Replacing the > sign here with < will sort in a non-increasing order.
+                    A.insert(A.begin() + j, elem);
+                    break;
+                }
+                j--;
             }
         }
-
+        cout << "Sorted Sequence\n";
+        for (int elem : A) cout << elem << ' ';
     }
 }
